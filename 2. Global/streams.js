950989c4216b95d8f0a07adcaf0475373f0a7896
hy-reza <hy.rezaalfanda@gmail.com>
2022-09-16 13:32:43 +0700

    Node Streams

---
+++ 2. Global/streams.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const readStream = fs.createReadStream('./docs/streamFile.txt', {encoding: 'utf8'})
+const writeStream = fs.createWriteStream('./docs/writeStream.txt')
+
+// readStream.on('data', (chunk) => {
+//   console.info('----New Chunk----');
+//   console.info(chunk);
+//   writeStream.write(chunk);
+// })
+
+
+//do the same thing
+readStream.pipe(writeStream)
